fe277f76ab7c5f4656e13d4bc9978f9a04652293
Pengyuan <pyhuang@umich.edu>
2021-01-25 01:31:42 -0500

    load pre-trained and get attention-map

--- scripts/load_speed_model.py
+++ scripts/load_speed_model.py
@@ -1,16 +1,3 @@
-#!/usr/bin/env python
-#
-# Copyright (c) 2019 Idiap Research Institute, http://www.idiap.ch/
-# Written by Angelos Katharopoulos <angelos.katharopoulos@idiap.ch>
-#
-
-"""Download the Swedish Traffic Signs dataset and create the Speed Limit Signs
-dataset from and train with attention sampling.
-
-NOTE: Swedish Traffic Signs dataset is provided from
-      https://www.cvl.isy.liu.se/research/datasets/traffic-signs-dataset/ .
-"""
-
 import argparse
 from collections import namedtuple
 from functools import partial
@@ -27,7 +14,7 @@ import keras.backend as K
 from keras.callbacks import Callback, ModelCheckpoint, LearningRateScheduler
 from keras.layers import Activation, BatchNormalization, Conv2D, \
     GlobalAveragePooling2D, MaxPooling2D, Dense, Input, add
-from keras.models import Model
+from keras.models import Model, load_model
 from keras.optimizers import SGD, Adam
 from keras.regularizers import l2
 from keras.utils import Sequence, plot_model
@@ -35,7 +22,7 @@ import numpy as np
 
 from ats.core import attention_sampling
 from ats.utils.layers import L2Normalize, ResizeImages, SampleSoftmax, \
-    ImageLinearTransform, ImagePan
+    ImageLinearTransform, ImagePan, ActivityRegularizer
 from ats.utils.regularizers import multinomial_entropy
 from ats.utils.training import Batcher
 
@@ -46,7 +33,10 @@ from tensorflow import ConfigProto
 from tensorflow import InteractiveSession
 from tensorflow.python import debug as tf_debug
 from keras.callbacks import TensorBoard
-from speed_utils import Sign, STS, SpeedLimits
+import tensorflow as tf
+from speed_utils import STS, Sign, SpeedLimits
+
+import matplotlib.pyplot as plt
 
 config = ConfigProto()
 config.gpu_options.allow_growth = True
@@ -55,44 +45,20 @@ sess = K.get_session()
 sess = tf_debug.LocalCLIDebugWrapperSession(sess)
 K.set_session(sess)
 
-class AttentionSaver(Callback):
-    """Save the attention maps to monitor model evolution."""
-    def __init__(self, output_directory, att_model, training_set, period=1):
-        self._dir = path.join(output_directory, "attention")
-        try:
-            os.mkdir(self._dir)
-        except FileExistsError:
-            pass
-        self._att_model = att_model
-        idxs = training_set.strided(10)
-        data = [training_set[i] for i in idxs]
-        self._X = np.array([d[0] for d in data])
-        self._Y = np.array([d[1] for d in data]).argmax(axis=1)
-        np.savetxt(
-            path.join(self._dir, "points.txt"),
-            np.array([[i, yi] for i, yi in zip(idxs, self._Y)]).astype(int),
-            fmt="%d"
-        )
-        self.period = period
-        self.epoch_since_last_epoch = 0
+def attention(x):
+    params = dict(
+        activation="relu",
+        padding="valid",
+        kernel_regularizer=l2(1e-5)
+    )
+    x = Conv2D(8, kernel_size=3, **params)(x)
+    x = Conv2D(16, kernel_size=3, **params)(x)
+    x = Conv2D(32, kernel_size=3, **params)(x)
+    x = Conv2D(1, kernel_size=3)(x)
+    x = MaxPooling2D(pool_size=8)(x)
+    x = SampleSoftmax(squeeze_channels=True, smooth=1e-4)(x)
 
-    def on_train_begin(self, *args):
-        _, _, x_low = self._att_model.predict(self._X)
-        for i, xi in enumerate(x_low):
-            self._imsave(path.join(self._dir, "{}.jpg").format(i), xi)
-
-    def on_epoch_end(self, e, logs):
-        self.epoch_since_last_epoch += 1
-        if self.epochs_since_last_save >= self.period:
-            self.epochs_since_last_save = 0
-            att, patches, _ = self._att_model.predict(self._X)
-            for i, att_i in enumerate(att):
-                np.save(path.join(self._dir, "att_{}_{}.npy").format(e, i), att_i)
-
-    def _imsave(self, filepath, x):
-        x = (x*255).astype(np.uint8)
-        imwrite(filepath, x)
-
+    return x
 
 def resnet(x, strides=[1, 2, 2, 2], filters=[32, 32, 32, 32]):
     """Implement a simple resnet."""
@@ -143,22 +109,6 @@ def resnet(x, strides=[1, 2, 2, 2], filters=[32, 32, 32, 32]):
     return y
 
 
-def attention(x):
-    params = dict(
-        activation="relu",
-        padding="valid",
-        kernel_regularizer=l2(1e-5)
-    )
-    x = Conv2D(8, kernel_size=3, **params)(x)
-    x = Conv2D(16, kernel_size=3, **params)(x)
-    x = Conv2D(32, kernel_size=3, **params)(x)
-    x = Conv2D(1, kernel_size=3)(x)
-    x = MaxPooling2D(pool_size=8)(x)
-    x = SampleSoftmax(squeeze_channels=True, smooth=1e-4)(x)
-
-    return x
-
-
 def get_model(outputs, width, height, scale, n_patches, patch_size, reg):
     x_in = Input(shape=(height, width, 3))
     x_high = ImageLinearTransform()(x_in)
@@ -180,7 +130,6 @@ def get_model(outputs, width, height, scale, n_patches, patch_size, reg):
         Model(inputs=x_in, outputs=[y]),
         Model(inputs=x_in, outputs=[att, patches, x_low])
     )
-
 
 def get_optimizer(args):
     optimizer = args.optimizer
@@ -204,7 +153,6 @@ def get_lr_schedule(args):
             return lr * 0.1
 
     return get_lr
-
 
 def main(argv):
     parser = argparse.ArgumentParser(
@@ -338,27 +286,68 @@ def main(argv):
     if args.resume:
         load_path = os.path.join(args.load_dir, "weights."+ str(args.load_epoch) + ".h5")
         model.load_weights(load_path)
+        model.trainable = False
+    else:
+        plot_model(model, to_file=path.join(args.output, "model.png"))
+
+        callbacks = [
+            AttentionSaver(args.output, att_model, training_set),
+            ModelCheckpoint(
+                path.join(args.output, "weights.{epoch:02d}.h5"),
+                save_weights_only=True
+            ),
+            LearningRateScheduler(get_lr_schedule(args)),
+            TensorBoard(log_dir=args.output+"/logs", histogram_freq=0, batch_size=args.batch_size, write_grads=True, write_images=True)
+        ]
+        model.fit_generator(
+            training_batched,
+            validation_data=test_batched,
+            epochs=args.epochs,
+            class_weight=class_weights,
+            callbacks=callbacks
+        )
+    # while len(model.layers) > 12:
+    #     model.layers.pop()
+    # model.summary()
+    # model.compile(
+    #     loss="categorical_crossentropy",
+    #     optimizer=get_optimizer(args),
+    #     metrics=["accuracy", "categorical_crossentropy"]
+    # )
+    intermediate_sample = model.layers[11].output
+    intermediate_attention_map = model.layers[10].output
+    model_b = Model(model.input, [intermediate_sample[0], intermediate_sample[1], intermediate_attention_map])
+    if not os.path.exists("test_patch"):
+        os.mkdir("test_patch")
+    for inputs, targets in test_batched:
+        #with tf.GradientTape() as tape:
+        #Forward pass.
+
+        patches, sampled_attention, attention_map = model_b.predict(inputs)
+        for b in range(patches.shape[0]):
+            imgs = patches[b]
+            fig, axs = plt.subplots(2)
+            axs[0].imshow(inputs[b])
+            axs[1].imshow(attention_map[b])
+            plt.savefig("test_patch/input%d.png" % b)
+            plt.clf()
+            for i in range(imgs.shape[0]):
+                patch = imgs[i]
+                plt.imshow(patch)
+                plt.savefig("test_patch/b%d_patch_%d.png" % (b, i))
+                plt.clf()    
+        attention_regularizer = multinomial_entropy(args.regularizer_strength)
+        attention_map = ActivityRegularizer(attention_regularizer)(attention_map)
 
 
-    plot_model(model, to_file=path.join(args.output, "model.png"))
-
-    callbacks = [
-        AttentionSaver(args.output, att_model, training_set),
-        ModelCheckpoint(
-            path.join(args.output, "weights.{epoch:02d}.h5"),
-            save_weights_only=True
-        ),
-        LearningRateScheduler(get_lr_schedule(args)),
-        TensorBoard(log_dir=args.output+"/logs", histogram_freq=0, batch_size=args.batch_size, write_grads=True, write_images=True)
-    ]
-    model.fit_generator(
-        training_batched,
-        validation_data=test_batched,
-        epochs=args.epochs,
-        class_weight=class_weights,
-        callbacks=callbacks
-    )
 
 
 if __name__ == "__main__":
     main(None)
+
+# new_model = load_model('/home/pyhuang/UM/attention/attention_sampling_keras/weights.1000.h5')
+# new_model.summary()
+# new_model.trainable = False    
+# while len(new_model.layers) > 12:
+#     new_model.layers.pop()
+# test_img = 
